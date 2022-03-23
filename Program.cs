9f84f99072c9f0396f02ba531513960ace98b202
fanis <fanis@DESKTOP-QBEAAG7>
2022-03-23 16:34:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Publisher publisher = new Publisher();
+
+publisher.OnKeyPressed += print_char;
+
+publisher.Run();
+
+
+void print_char(object? sender, char ch) 
+{
+    Console.WriteLine("\bThe symbol has been entered: " + ch);
+}
+
+class Publisher 
+{
+    public void Run() 
+    {
+        char ch;
+
+        while (true)
+        {
+            var read_key = Console.ReadKey();
+            ch = read_key.KeyChar;
+            if (ch != 'c' && ch != 'C')
+            {
+                OnKeyPressed?.Invoke(this, ch);
+            }
+            else
+            {
+                Console.Write("\b");
+                break;
+            }
+        }
+    }
+    public event EventHandler<char>? OnKeyPressed;
+}
